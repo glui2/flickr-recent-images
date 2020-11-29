c442d8878b113f46c70d3402e3290b0e09a688c5
Gavin <gavin.lui@gmail.com>
2020-11-30 01:00:39 +1100

    display recent images from API call

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,43 @@
     </v-app-bar>
     <v-main>
       <p>Number of images: {{ numberOfImages }}</p>
-      <ImageGrid />
+      
+        <ImageSquare     
+          v-for="imgObj in images" 
+          :key="imgObj.id"
+          :farm="imgObj.farm" 
+          :server="imgObj.server"
+          :id="imgObj.id"
+          :secret="imgObj.secret" 
+          :title="imgObj.title" 
+        />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import ImageGrid from "./components/ImageGrid";
 import axios from "axios";
+import ImageSquare from './components/ImageSquare.vue';
 
 export default {
   name: "App",
 
   components: {
-    ImageGrid
+    ImageSquare
   },
   mounted: function() { 
     axios.post("https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=d7f66852aed44008ce15c75baaed617a&format=json&nojsoncallback=1")
-    .then(response => { 
+    .then(response => {
+      console.log(response.data); 
       this.numberOfImages = response.data.photos.photo.length;
+      this.images = response.data.photos.photo;
     })
   },
 
   data: () => ({
     //
-    numberOfImages: 0
+    numberOfImages: 0,
+    images: []
   })
 };
 </script>
---
+++ src/components/ImageSquare.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card max-width="300">
+    <v-img :src="imgSrc"></v-img>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ImageSquare",
+  data: function () {
+    return { imgSrc: "http://farm" + this.farm + ".staticflickr.com/" + this.server + "/" + this.id + "_" + this.secret + ".jpg" }
+  },
+  props: {
+    farm: String, 
+    server: String,
+    id: String,
+    secret: String, 
+    title: String
+  }
+};
+</script>
