29ed8bddb9e9107f26a755410d98eda82e4f1b24
Gavin <gavin.lui@gmail.com>
2020-12-01 16:56:07 +1100

    add basic search function using search api

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <v-app>
     <v-app-bar app color="primary" dark>
             <h3>Number of images: {{ numberOfImages }}</h3>
+            <v-spacer></v-spacer>
+            <v-text-field style="margin: auto" label="Search recent images" solo v-model="searchInputData" placeholder="Search for an image" v-on:keyup.enter="searchInputData ? searchInput(searchInputData) : searchRecent()"></v-text-field>
     </v-app-bar>
     <v-main>
       <v-container container>
@@ -31,18 +33,31 @@ export default {
     ImageSquare
   },
   mounted: function() { 
-    axios.post("https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=" + process.env.VUE_APP_FLICKR_KEY + "&format=json&nojsoncallback=1")
-    .then(response => {
-      console.log(response.data); 
-      this.numberOfImages = response.data.photos.photo.length;
-      this.images = response.data.photos.photo;
-    })
+    this.searchRecent();
   },
-
   data: () => ({
     //
     numberOfImages: 0,
-    images: []
-  })
+    images: [],
+    searchInputData: '',
+  }),
+  methods: {
+    searchRecent: function() {
+      axios.post("https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=" + process.env.VUE_APP_FLICKR_KEY + "&format=json&nojsoncallback=1")
+      .then(response => {
+        console.log(response.data); 
+        this.numberOfImages = response.data.photos.photo.length;
+        this.images = response.data.photos.photo;
+      })
+    },
+    searchInput: function (stringInput) {
+      axios.post("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + process.env.VUE_APP_FLICKR_KEY + "&text=" + stringInput + "&format=json&nojsoncallback=1")
+      .then(response => {
+        console.log(response.data); 
+        this.numberOfImages = response.data.photos.photo.length;
+        this.images = response.data.photos.photo;
+      })
+    }
+  }
 };
 </script>
